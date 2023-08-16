e27673936be7ec09ff8e0fd125ce14e3a92e2e60
Kaynnan <kaynnanx1@ggmail.com>
2023-08-16 10:06:47 -0300

    [ADD] egd_sale_blanket_order_custom: add new module

---
+++ egd_sale_blanket_order_custom/wizard/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2023 - TODAY, Kaynnan Lemes <kaynnan.lemes@escodoo.com.br>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+from . import sale_create_order_plan
+from . import sale_make_planned_order
