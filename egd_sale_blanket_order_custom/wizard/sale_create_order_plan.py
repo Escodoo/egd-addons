1dfd49f4d48a0c64a8204915e3fe733d3bcc8e4d
Marcel Savegnago <marcel.savegnago@gmail.com>
2023-09-20 12:43:22 -0300

    [RFC] egd_sale_blanket_order_custom

--- egd_sale_blanket_order_custom/wizard/sale_create_order_plan.py
+++ egd_sale_blanket_order_custom/wizard/sale_create_order_plan.py
@@ -1,5 +1,6 @@
 # Copyright 2023 - TODAY, Kaynnan Lemes <kaynnan.lemes@escodoo.com.br>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
 from odoo import _, api, fields, models
 from odoo.exceptions import ValidationError
 
