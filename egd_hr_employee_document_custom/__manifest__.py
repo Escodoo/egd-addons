a50b48308222b8ec287a5b51f176bcfeb56d1c56
Kaynnan Lemes <kaynnanx1@gmail.com>
2023-10-16 14:58:39 -0300

    [ADD] egd_hr_employee_document_custom: add new module

---
+++ egd_hr_employee_document_custom/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Egd Hr Employee Document Custom",
+    "summary": """
+        EGD HR Employee Document Custom""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/egd-addons",
+    "depends": ["hr_employee_document"],
+    "data": [
+        "views/ir_attachment.xml",
+    ],
+}
