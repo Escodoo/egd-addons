eeacdf9c7171e693674d27cad6034a607a4043fc
Kaynnan Lemes <kaynnanx1@gmail.com>
2023-10-20 11:56:20 -0300

    [RFC] egd_hr_expense_advance_clearing_custom: refactor name and attrs in view

---
+++ egd_hr_expense_advance_clearing_custom/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "EGD Hr Expense Advance Clearing Custom",
+    "summary": """
+        EGD HR Expense Advance Clearing Custom""",
+    "version": "14.0.1.0.1",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/egd-addons",
+    "depends": ["hr_expense_advance_clearing"],
+    "data": [
+        "views/hr_expense.xml",
+    ],
+}
