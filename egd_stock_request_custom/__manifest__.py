ea567ef758d3f671de97f0a16627dde5d749df99
Marcel Savegnago <marcel.savegnago@gmail.com>
2023-11-11 15:55:20 -0300

    [ADD] egd_stock_request_custom: new module - add compare target with standard_price - TESTES

---
+++ egd_stock_request_custom/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Egd Stock Request Custom",
+    "summary": """
+        EGD Stock Request Custom""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/egd-addons",
+    "depends": ["stock_request_analytic", "egd_sale_blanket_order_custom"],
+    "data": [
+        "views/stock_request_order.xml",
+        "views/stock_request.xml",
+    ],
+    "demo": [],
+}
