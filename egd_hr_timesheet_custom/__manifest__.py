e7e3bfcac10dae7695979b7659cfd9cd7cc97345
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2024-02-05 13:39:38 -0300

    [ADD] egd_hr_timesheet_custom: add new module

---
+++ egd_hr_timesheet_custom/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 - TODAY Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Egd Hr Timesheet Custom",
+    "summary": """
+        Egd Hr Timesheet Custom""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/egd-addons",
+    "depends": ["hr", "hr_timesheet"],
+    "data": [],
+    "demo": [],
+}
