4d5043a120369120dc9e460a2a1659c60769aec6
Matthwhy <marquesmathw@gmail.com>
2024-02-29 23:47:49 -0300

    [ADD] egd_work_evaluation_mandatory: add new module

---
+++ egd_work_evaluation_mandatory/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Egd Work Evaluation Mandatory",
+    "summary": """
+        This module creates a field where you select whether work evaluation is mandatory.""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/egd-addons",
+    "depends": ["purchase", "supplier_quality_index"],
+    "data": [
+        "views/res_partner.xml",
+        "views/purchase_view.xml",
+    ],
+}
---
+++ egd_work_evaluation_mandatory/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module creates a field where you select whether work evaluation is mandatory, and inserts a view in the purchasing module where you can monitor which suppliers have mandatory evaluations.
