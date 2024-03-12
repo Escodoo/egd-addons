5c6a887b8f194225443e47c5bb3531c566849014
Matthwhy <marquesmathw@gmail.com>
2024-03-12 14:51:34 -0300

    [ADD] egd_cnh_date_expiry: add new module

---
+++ egd_cnh_date_expiry/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Egd CNH Expiry",
+    "summary": """
+        Module to track driver's license expiration""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/egd-addons",
+    "depends": ["l10n_br_hr", "fleet"],
+    "data": ["views/hr_employee.xml", "views/fleet_vehicle_menu.xml"],
+}
