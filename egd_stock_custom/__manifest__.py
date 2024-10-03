8bd7067345e0b8429ad37c8271f38ebe88e00bc5
kaynnan <kaynnanx1@gmail.com>
2024-10-03 17:43:29 -0300

    [ADD] egd_stock_custom: add new module

---
+++ egd_stock_custom/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2024 - TODAY, Escodoo
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Egd Stock Custom",
+    "summary": """
+        EGD Stock Custom""",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Escodoo",
+    "website": "https://github.com/Escodoo/egd-addons",
+    "depends": ["stock"],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+}
