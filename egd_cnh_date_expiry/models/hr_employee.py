f848d33e198e24d6d072c99ca61dbdfb3d8f963c
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2024-10-07 15:21:36 -0300

    [RFC] egd_cnh_date_expiry: refactor module

--- egd_cnh_date_expiry/models/hr_employee.py
+++ egd_cnh_date_expiry/models/hr_employee.py
@@ -1,45 +1,54 @@
 # Copyright 2024 - TODAY, Matheus Marques <matheus.marques@escodoo.com.br>
+# Copyright 2024 - TODAY, Wesley Oliveira <wesley.oliveira@escodoo.com.br>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from datetime import datetime, timedelta
+from datetime import date
 
-from odoo import models
+from odoo import api, fields, models
 
 
 class HrEmployee(models.Model):
 
     _inherit = "hr.employee"
 
-    def _get_employee_expirations(self):
-        expiration_date_threshold = datetime.now() + timedelta(days=30)
-        employees = self.env["hr.employee"].search(
-            [("expiration_date", "<=", expiration_date_threshold)]
-        )
-        expirations = []
-        for employee in employees:
-            days_until_expiration = (
-                employee.expiration_date - datetime.now().date()
-            ).days
-            expirations.append(
-                {"name": employee.name, "days_until_expiration": days_until_expiration}
-            )
-        return expirations
+    cnh_days_to_expire = fields.Integer(
+        string="CNH Days to Expire",
+        compute="_compute_cnh_days_to_expire",
+        readonly=True,
+    )
+    cnh_expiry_state = fields.Selection(
+        selection=[
+            ("valid", "Valid"),
+            ("expiring_soon", "Expiring Soon"),
+            ("expired", "Expired"),
+            ("no_cnh", "No CNH"),
+        ],
+        string="CNH Expiry State",
+        compute="_compute_cnh_expiry_state",
+        store=True,
+        readonly=True,
+    )
 
-    def action_view_employee_expirations(self):
-        expirations = self._get_employee_expirations()
-        return {
-            "name": "Employees with Expiration Date Approaching",
-            "view_mode": "tree",
-            "res_model": "employee.expiration",
-            "type": "ir.actions.act_window",
-            "context": {"create": False},
-            "domain": [],
-            "view_id": False,
-            "views": [(False, "tree")],
-            "target": "current",
-            "readonly": True,
-            "limit": 80,
-            "auto_search": False,
-            "auto_refresh": 1,
-            "expirations": expirations,
-        }
+    @api.depends("expiration_date")
+    def _compute_cnh_days_to_expire(self):
+        today = date.today()
+        for employee in self:
+            if employee.expiration_date:
+                days = (employee.expiration_date - today).days
+                employee.cnh_days_to_expire = days
+            else:
+                employee.cnh_days_to_expire = 0
+            employee._compute_cnh_expiry_state()
+
+    @api.depends("driver_license", "cnh_days_to_expire")
+    def _compute_cnh_expiry_state(self):
+        for employee in self:
+            if not employee.driver_license:
+                employee.cnh_expiry_state = "no_cnh"
+            else:
+                if employee.cnh_days_to_expire < 0:
+                    employee.cnh_expiry_state = "expired"
+                elif employee.cnh_days_to_expire <= 30:
+                    employee.cnh_expiry_state = "expiring_soon"
+                else:
+                    employee.cnh_expiry_state = "valid"
