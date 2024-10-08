d5678313081fd324aff78aeb7d93b4dfcda49deb
Wesley Oliveira <wesleygabrieldeoliveira@gmail.com>
2024-10-08 15:48:49 -0300

    [RFC] egd_insurance_date_expiry: refactor module

--- egd_insurance_date_expiry/models/fleet_vehicle.py
+++ egd_insurance_date_expiry/models/fleet_vehicle.py
@@ -1,27 +1,55 @@
 # Copyright 2024 - TODAY, Matheus Marques <matheus.marques@escodoo.com.br>
+# Copyright 2024 - TODAY, Wesley Oliveira <wesley.oliveira@escodoo.com.br>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from datetime import datetime, timedelta
+from datetime import date
 
 from odoo import api, fields, models
 
 
 class FleetVehicle(models.Model):
+
     _inherit = "fleet.vehicle"
 
-    insurance_date_expiry = fields.Date(string="Insurance expiry date")
-    insurance_near_expiry = fields.Boolean(
-        string="insurance Near Expiry", compute="_compute_insurance_near_expiry"
+    insurance_date_expiry = fields.Date(string="Insurance Expiry Date")
+    insurance_days_to_expire = fields.Integer(
+        string="Insurance Days to Expire",
+        compute="_compute_insurance_days_to_expire",
+        readonly=True,
+    )
+    insurance_expiry_state = fields.Selection(
+        selection=[
+            ("valid", "Valid"),
+            ("expiring_soon", "Expiring Soon"),
+            ("expired", "Expired"),
+            ("no_insurance", "No Insurance"),
+        ],
+        string="Insurance Expiry State",
+        compute="_compute_insurance_expiry_state",
+        store=True,
+        readonly=True,
     )
 
     @api.depends("insurance_date_expiry")
-    def _compute_insurance_near_expiry(self):
-        for vehicle in self:
-            if vehicle.insurance_date_expiry:
-                expiry_date = fields.Date.from_string(vehicle.insurance_date_expiry)
-                if expiry_date - datetime.now().date() <= timedelta(days=30):
-                    vehicle.insurance_near_expiry = True
-                else:
-                    vehicle.insurance_near_expiry = False
+    def _compute_insurance_days_to_expire(self):
+        today = date.today()
+        for fleet in self:
+            if fleet.insurance_date_expiry:
+                days = (fleet.insurance_date_expiry - today).days
+                fleet.insurance_days_to_expire = days
             else:
-                vehicle.insurance_near_expiry = False
+                fleet.insurance_days_to_expire = 0
+            fleet._compute_insurance_expiry_state()
+
+    @api.depends("insurance_date_expiry", "insurance_days_to_expire")
+    def _compute_insurance_expiry_state(self):
+        for fleet in self:
+            if not fleet.insurance_date_expiry:
+                fleet.insurance_expiry_state = "no_insurance"
+            else:
+                if fleet.insurance_days_to_expire < 0:
+                    fleet.insurance_expiry_state = "expired"
+                elif fleet.insurance_days_to_expire <= 30:
+                    fleet.insurance_expiry_state = "expiring_soon"
+                else:
+                    fleet.insurance_expiry_state = "valid"
