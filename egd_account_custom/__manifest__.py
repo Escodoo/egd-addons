2cfa285ca00335d2818cf00ffcf65493518b1013
CristianoMafraJunior <cristiano.mafracontanto@gmail.com>
2025-12-09 11:17:08 -0300

    [MIG] egd_account_custom: Migration to 16.0

--- egd_account_custom/__manifest__.py
+++ egd_account_custom/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "EGD Account Custom",
-    "version": "14.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "Escodoo",
     "website": "https://github.com/Escodoo/egd-addons",
